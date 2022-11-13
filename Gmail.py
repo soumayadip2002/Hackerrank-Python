3d2d8d35253b8d00ddb56f4450df58841c02d417
soumayadip2002 <soumayadipsaha2002@gmail.com>
2022-11-13 20:14:36 +0530

    Main

---
+++ Gmail.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.message import EmailMessage
+
+#Create an Object that will hold all email message details
+
+message = EmailMessage()
+
+EmailContent = 'Email test from Python'
+SenderDetails = 'soumayadipsaha2002@gmail.com'
+ReceiverDetails = 'sinuroy696@gmail.com'
+#ignorecrowdweb@gmail.com
+
+message['Subject'] = EmailContent
+message['From'] = SenderDetails
+message['To'] = ReceiverDetails
+
+message.set_content('Hello from python')
+
+EmailSmtp = 'smtp.gmail.com'
+server = smtplib.SMTP(EmailSmtp, '587')
+server.ehlo()
+server.starttls()
+
+emailpass = 'Soumya@2001'
+server.login(SenderDetails, emailpass)
+server.send_message(message)
+server.quit()
